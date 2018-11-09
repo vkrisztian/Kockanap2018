611c4cb48b7b5cce3491a1fd424b7a08b7e27451
v.vori <v.vori@stud.uni-obuda.hu>
2018-11-09 17:48:25 +0100

    start upd server

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+             // simple geometry
+            UDPServer server = new UDPServer();
+            HandleDataClass hdc = new HandleDataClass();
+
+
+            Thread serverThread = new Thread(() => server.Listen());
+            serverThread.Start();
+
+            Thread dataHandlerThread = new Thread(() =>
+            hdc.SubscribeToEvent(server));
+            dataHandlerThread.Start();
+
+            while(true)
+            {
+                Thread.Sleep(100);
+            }
         }
     }
 }
